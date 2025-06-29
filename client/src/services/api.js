ff97343568d70a66b784c94e74f368889c43fa71
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2025-06-29 23:11:34 +0530

    Update api.js add DB

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL
+    ? `${import.meta.env.VITE_BACKEND_URL}/api`
+    : "http://localhost:5000/api",
 });
-
 export const signup = (formData) => API.post("/auth/signup", formData);
 export const login = (formData) => API.post("/auth/login", formData);
